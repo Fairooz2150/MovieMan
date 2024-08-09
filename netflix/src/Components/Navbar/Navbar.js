76e042764c4792e128803791c9bbadea5f5861bf
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-09 23:32:34 +0530

    Added Logo and User Avatar images

--- netflix/src/Components/Navbar/Navbar.js
+++ netflix/src/Components/Navbar/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='navbar'>
-      
+      <img src="netflix-logo.png" alt="Netflix" />
+      <img src="avatar.png" alt="Avatar" />
     </div>
   )
 }
