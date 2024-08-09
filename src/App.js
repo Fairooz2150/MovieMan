a37e486d93f31e05bc884489e89515534be23092
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-10 01:14:23 +0530

    Add dummy movie row

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css'
-import Banner from "./Components/Navbar/Banner/Banner";
+import Banner from "./Components/Banner/Banner";
+import Movies from "./Components/Movies/Movies";
 function App() {
   return (
     <div className="App">
      <Navbar/>
       <Banner/>
+      <Movies/>
     </div>
   );
 }
---
+++ src/Components/Movies/Movies.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Movies.css'
+
+function Movies() {
+  return (
+    <div className='row'>
+        <h2>Title</h2>
+        <div className='posters'>
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        <img className='poster' src="images/posters.jpg" alt="Poster" />
+        </div>
+    </div>
+  )
+}
+
+export default Movies
