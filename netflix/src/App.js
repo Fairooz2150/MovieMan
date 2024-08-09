c17eacce7f126e2ee965adc3c5811aa8513e83d7
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-09 23:31:19 +0530

    Initial commit of the project

---
+++ netflix/src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
