592a786d5ab37b21629a23e51fb10313b3a38d75
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-12 17:31:11 +0530

    Changed trending's url in urls.js

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,4 @@
 import { API_KEY } from "./constants/constants"
 
-export const trending = `discover/tv?api_key=${API_KEY}&with_networks=213`
+export const trending = `trending/all/week?api_key=${API_KEY}&language=en-US`
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
