f4c83bdeba09e2aa66a488f6da98d9f24b3c35b6
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-12 15:38:41 +0530

    Set Banner changing in random order on refreshing

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -9,7 +9,7 @@ function Banner() {
 useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
       console.log(response.data.results[0]);
-      setMovie(response.data.results[0])
+      setMovie(response.data.results.sort(function (a, b) { return 0.5 - Math.random() })[0])
     })
 }, [])
 
