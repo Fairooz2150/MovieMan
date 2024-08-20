bf8e1b780a6936eb5f95cc3ac34317e22d4a8da6
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-21 02:35:41 +0530

    Add more genres

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css'
-import { trending,action } from "./urls";
+import { trending,action,romance,horror,comedy,documentaries } from "./urls";
 import Banner from "./Components/Banner/Banner";
 import Movies from "./Components/Movies/Movies";
 function App() {
@@ -10,8 +10,12 @@ function App() {
     <div className="App">
      <Navbar/>
       <Banner/>
-      <Movies title='Trending' url={trending} />
-      <Movies title='Action' url={action} />
+      <Movies title='Trending Now' url={trending} />
+      <Movies title='Laugh Out Loud' url={comedy} />
+      <Movies title='Action Thrillers' url={action} />
+      <Movies title='Horror Essentials' url={horror} />
+      <Movies title='Love Stories' url={romance} />
+      <Movies title='Uncovering Truths' url={documentaries} />
     </div>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -4,12 +4,10 @@ export const trending = `trending/all/week?api_key=${API_KEY}&language=en-US`
 
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
 
-export const hot = `discover/tv?api_key=${API_KEY}&with_networks=213`
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
 
-export const Comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
 
-export const Horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
+export const romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`
 
-export const Romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`
-
-export const Documentaries = `discover/movie?api_key=${API_KEY}&with_genres=99`
+export const documentaries = `discover/movie?api_key=${API_KEY}&with_genres=99`
