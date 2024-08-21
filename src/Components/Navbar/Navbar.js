71933ede9312fb8ec9d7c9c9805b5441943deb0e
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-22 02:44:57 +0530

    Added a Back to Top button on Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Navbar.css";
 import About from '../About/About';
 
 function Navbar() {
   const [showAbout, setShowAbout] = useState(false);
+  const [showTopButton, setShowTopButton] = useState(false);
 
   const toggleAbout = () => {
     setShowAbout(!showAbout);
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 300) {
+        setShowTopButton(true);
+      } else {
+        setShowTopButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -20,6 +41,12 @@ function Navbar() {
         />
       </div>
       {showAbout && <About onClose={toggleAbout} />}
+
+      {showTopButton && (
+        <button className="top-button" onClick={scrollToTop}>
+          Back to Top <i class="bi bi-arrow-up-circle"></i>
+        </button>
+      )}
     </>
   );
 }
