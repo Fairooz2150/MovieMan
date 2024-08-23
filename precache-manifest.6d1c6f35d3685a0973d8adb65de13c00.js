2202740ffcced3060bfd0f81a86575c55d284718
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-23 20:25:58 +0530

    Updates

---
+++ precache-manifest.6d1c6f35d3685a0973d8adb65de13c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9858992c7d66e04faeaf02019dd332d7",
+    "url": "/MovieMan/index.html"
+  },
+  {
+    "revision": "e838e94e6a3da608e92f",
+    "url": "/MovieMan/static/css/main.d5b738c1.chunk.css"
+  },
+  {
+    "revision": "b6c496910565c22cd44f",
+    "url": "/MovieMan/static/js/2.1ff6c386.chunk.js"
+  },
+  {
+    "revision": "a5eff27805333dc79e6a0e76b90a434d",
+    "url": "/MovieMan/static/js/2.1ff6c386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e838e94e6a3da608e92f",
+    "url": "/MovieMan/static/js/main.9cc26c7b.chunk.js"
+  },
+  {
+    "revision": "1f80c7f6668293fa21ef",
+    "url": "/MovieMan/static/js/runtime-main.fca4973f.js"
+  }
+]);
