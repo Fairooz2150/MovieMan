cc7430828014fc3dcb3e6e57cd56adfda634ce78
Muhammad Fairooz <144594632+Fairooz2150@users.noreply.github.com>
2024-08-23 17:01:29 +0530

    API Key changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl ='https://api.themoviedb.org/3';
-export const API_KEY ='0e907cb1df1a471dedc50076817419a1';
+export const API_KEY ='8b11869f895b51d9ca33a7aba044360e';
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
